54ba9cb7137129f1330ed010bd1b7aa9fa17a802
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-19 13:11:24 -0300

    test de de vistas

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,10 +1,14 @@
 <?php
     namespace Controllers;
 
+    use DAO\CompanyDAO;
+    use DAO\StudentDAO;
+
     class HomeController
     {
         public function viewHomeAdmin(){
        
+            /*
             if($this->adminIsLogged()){
       
                 require_once(VIEWS_PATH."header.php");
@@ -15,7 +19,7 @@
               echo '<script>alert("No tiene acceso para entrar en esta pagina");</script>';
               $this->Index();
             }
-         
+            */
         }
         
 
@@ -25,13 +29,40 @@
         public function Index()
         {
         
-            require_once(VIEWS_PATH."header.php");
-            require_once(VIEWS_PATH."nav.php");
-            require(VIEWS_PATH."home.php");
-            //require(VIEWS_PATH."signUp.php");
-            //require(VIEWS_PATH."signIn.php");
-            require_once(VIEWS_PATH."footer.php");
-        
+            //require_once(VIEWS_PATH."header.php");
+          // require_once(VIEWS_PATH."nav.php");
+            //require(VIEWS_PATH."home.php");
+           // require_once(VIEWS_PATH."signUp.php");
+            
+            
+            
+               // require_once(VIEWS_PATH."nav-admin.php");
+                //require(VIEWS_PATH.'index.php');
+            
+             //ANDA JOYA NO TOCAR
+            //require_once(VIEWS_PATH."signIn.php");
+            
+            
+            
+            //require_once(VIEWS_PATH."footer.php");
+
+
+            //require_once(VIEWS_PATH."company-add.php");
+
+            
+            /* // COMPANY ANDA TODO
+            $companyDAO = new CompanyDAO;
+            $companyList = $companyDAO->GetAll();
+            require_once(VIEWS_PATH."company-list.php");
+            */
+            
+            
+            /*// STUDENT ANDA PERFECTO
+            $studentDAO = new StudentDAO();
+            $studentList = array();
+            $studentList = $studentDAO->GetAll();
+            var_dump($studentList);
+            */
         }    
 
         //============================================================
