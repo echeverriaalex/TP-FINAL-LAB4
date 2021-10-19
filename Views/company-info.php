e4e551d55a3996499f4bed7aca39b6a57b75754b
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-19 12:16:58 -0300

    vista para ver info de un determinada empresa

---
+++ Views/company-info.php
@@ -0,0 +1,10 @@
+<section>
+    <article>
+        
+        <h2> <?php echo $company->getName(); ?></h2>
+        <?php echo $company->getAddress(); ?>
+        <?php echo $company->getPhone(); ?> 
+        <?php echo $company->getCuit(); ?> 
+
+    </article>
+</section>
