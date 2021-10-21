ac9a8fb0d9f8f2bd00079f43eac25190b213acc7
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-20 23:05:27 -0300

    acomo la vista para editar

--- Views/company-edit.php
+++ Views/company-edit.php
@@ -1,37 +1,35 @@
 <?php
     require_once('nav-admin.php');
-
-    use Models\Company;
-    $company = new Company();
 ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
                <h2 class="mb-4">Edit Company</h2>
-               <form action="<?php echo FRONT_ROOT ?>Company/Add" method="post" class="bg-light-alpha p-5">
+               <form action="<?php echo FRONT_ROOT ?>Company/Edit" method="post" class="bg-light-alpha p-5">
                     <div class="row">  
                          <div class="col-lg-4">
                               <div class="form-group">
+                                   <input type="hidden" name="currentName" value="<?php echo $name; ?>">
                                    <label for="">Name</label>
-                                   <input type="text" name="name" value="<?php echo $company->getName();?>" class="form-control">
+                                   <input type="text" name="name" value="<?php echo $name; ?>" class="form-control">
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Address</label>
-                                   <input type="text" name="address" value="<?php echo $company->getAddress();?>" class="form-control">
+                                   <input type="text" name="address" value="<?php echo $address; ?>" class="form-control">
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Phone</label>
-                                   <input type="text" name="phone" value="<?php echo $company->getPhone();?>" class="form-control">
+                                   <input type="text" name="phone" value="<?php echo $phone ;?>" class="form-control">
                               </div>
                          </div>
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Cuit</label>
-                                   <input type="text" name="cuit" value="<?php echo $company->getCuit();?>" class="form-control">
+                                   <input type="text" name="cuit" value="<?php echo $cuit; ?>" class="form-control">
                               </div>
                          </div>
                     </div>
