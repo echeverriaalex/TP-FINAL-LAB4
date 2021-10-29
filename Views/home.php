b05599783385e9afb85a0c76d72f1dde120818c7
echeverriaalex <aleex.naahuel@outlook.com>
2021-10-29 16:46:44 -0300

    agrego nav y corrijo el codigo

--- Views/home.php
+++ Views/home.php
@@ -1,47 +1,45 @@
+<?php
+    require_once(VIEWS_PATH."nav.php");
+?>
 <body class= "home">
   <div class ="container mt-5">
     <div class= "row">
-
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img class="img-fluid" src="Views/img/equipo.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5> <mark>Bienvenidos a LINKEDON</mark></h5>
-        <p> <mark>En esta wed vas a poder postularte para tu primer trabajo IT</mark>.</p>
+      <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <img class="img-fluid" src= "<?php echo FRONT_ROOT.VIEWS_PATH ?>/img/equipo.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+          <h5> <mark>Bienvenidos a LINKEDON</mark></h5>
+          <p> <mark>En esta wed vas a poder postularte para tu primer trabajo IT</mark>.</p>
+      </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img class="img-fluid" src= "<?php echo FRONT_ROOT.VIEWS_PATH ?>/img/equipo2.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h6> <mark>Ingresa a la wed registrate y tu busqueda laboral comenza YA</mark> </h6>
+          <p> <mark>Solo tenes que completar tus datos personales y buscar la empresa que mas te guste</mark></p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="img-fluid" src= "<?php echo FRONT_ROOT.VIEWS_PATH ?>/img/equipo3.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h6> <mark>No tengas miendo y elegi siempre a LINKEDON como tu lugar de confianza</mark> </h6>
+          <p> <mark>Pensa bonito y sucedera bonito</mark></p>
+        </div>
       </div>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img class="img-fluid" src="Views/img/equipo2.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h6> <mark>Ingresa a la wed registrate y tu busqueda laboral comenza YA</mark> </h6>
-        <p> <mark>Solo tenes que completar tus datos personales y buscar la empresa que mas te guste</mark></p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img class="img-fluid" src="Views/img/equipo3.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h6> <mark>No tengas miendo y elegi siempre a LINKEDON como tu lugar de confianza</mark> </h6>
-        <p> <mark>Pensa bonito y sucedera bonito</mark></p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    
-    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
 </body>
-
