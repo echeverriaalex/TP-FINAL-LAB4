823e16de45c6b7e3fb1d54da2f5c75abb7ae7352
echeverriaalex <aleex.naahuel@outlook.com>
2021-11-01 20:06:44 -0300

    agrego mensaje en caso de que no haya resultados de busqueda

--- Views/company-info.php
+++ Views/company-info.php
@@ -6,7 +6,9 @@
         <section id="listado" class="mb-5">
             <div class="container">
                 <div class="list-group">
-
+                    <?php
+                        if($company->getName() != null){
+                    ?>
                     <div class="col-lg-4">
                         <h2 class="text-light"> <?php echo $company->getName() ?> </h2>
                     </div>
@@ -21,7 +23,18 @@
 
                     <div class="col-lg-4">
                         <a class="list-group-item list-group-item-action list-group-item-danger"> Cuit <?php echo $company->getCuit();?></a>
+                    </div>                    
+                    <?php
+                        }
+                        else{
+                    ?>
+                    <div class="col-lg-4">
+                        <h2 class="text-light"> <?php echo "No results found"; ?> </h2>
                     </div>
+
+                    <?php
+                        }
+                    ?>
                 </div>
             </div>
         </section> 
