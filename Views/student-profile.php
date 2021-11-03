d19245d9486c686a93cee6e1941490791c4526ba
echeverriaalex <aleex.naahuel@outlook.com>
2021-11-03 13:55:08 -0300

    muestro mas informacion del student

--- Views/student-profile.php
+++ Views/student-profile.php
@@ -3,22 +3,16 @@
         <section id="listado" class="mb-5">
             <div class="container">
                 <div class="list-group">
-
-                    <div class="col-lg-4">
-                        <h2 class="text-light"> <?php echo $student->getFirstName(); ?> </h2>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <a class="list-group-item list-group-item-action list-group-item-secondary"> Phone <?php echo $student->getPhoneNumber(); ?> </a>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <a class="list-group-item list-group-item-action list-group-item-success"> DNI <?php echo $student->getDni();?></a>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <a class="list-group-item list-group-item-action list-group-item-danger"> ID <?php echo $student->getStudentId();?></a>
-                    </div>
+                    <div class="col-lg-4"> <h2 class="text-light"> <?php echo $student->getFirstName()." ".$student->getLastName();?> </h2></div>
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-danger"> ID <?php echo $student->getStudentId();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> Career ID <?php echo $student->getCareerId();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> DNI <?php echo $student->getDni();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> File number <?php echo $student->getFileNumber();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> Gender <?php echo $student->getGender();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> Birth date <?php echo $student->getBirthDate();?></a> </div>                    
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> Email <?php echo $student->getEmail();?></a> </div>
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-secondary"> Phone <?php echo $student->getPhoneNumber(); ?> </a> </div>
+                    <div class="col-lg-4"> <a class="list-group-item list-group-item-action list-group-item-success"> Status <?php if($student->getActive()) echo "Active"; else echo "No active"; ?></a> </div>
                 </div>
             </div>
         </section> 
