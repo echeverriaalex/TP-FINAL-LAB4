3c35bad1d1d82f50870a8db94b62fb87db00afb1
echeverriaalex <aleex.naahuel@outlook.com>
2021-11-06 10:01:19 -0300

    modificar para agregar jobs offers

---
+++ Views/joboffer-add.php
@@ -0,0 +1,37 @@
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Add Job Offer</h2>
+               <form action="<?php echo FRONT_ROOT ?>JobOffer/Add" method="post" class="bg-light-alpha p-5">
+                    <div class="row">  
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Name</label>
+                                   <input type="text" name="name" value="" class="form-control text-light" autofocus>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Address</label>
+                                   <input type="text" name="address" value="" class="form-control text-light">
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">CUIT</label>
+                                   <input type="text" name="cuit" value="" class="form-control text-light">
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Phone</label>
+                                   <input type="text" name="phone" value="" class="form-control text-light">
+                              </div>
+                         </div>
+                    </div>
+                    <button type="submit" class="btn btn-dark ml-auto d-block">Create job offer</button>
+               </form>
+          </div>
+          
+     </section>
+</main>
