f47e50dbeac1ec68e5278902e058c5f9aa802ab3
echeverriaalex <aleex.naahuel@outlook.com>
2021-11-09 02:04:09 -0300

    correccion

--- Views/administrator-add.php
+++ Views/administrator-add.php
@@ -13,7 +13,7 @@
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="">Password</label>
-                                   <input type="text" name="password" class="form-control text-light">
+                                   <input type="password" name="password" class="form-control text-light">
                               </div>
                          </div>
                     </div>
